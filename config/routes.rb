d2fbfc476dd02fde203d4f9bc9dda02e282a96b4
Kalisp20 <hanane.duissi@yahoo.com>
2018-10-30 23:46:07 +0100

    connection

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  #verbe '/url/:url', to: 'controller#méthode'
-  get '/:users', to: 'new'
-  get '/:users', to: 'create'
-  #get '/user/:user', to: 'user#new'
-  #get '/user/:user', to: 'user#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'user#show'
+  resources :users
 end
