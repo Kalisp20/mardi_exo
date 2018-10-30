964dad3ea2ffa1145bcc34ccc246c15e17d6ecce
Kalisp20 <hanane.duissi@yahoo.com>
2018-10-31 00:32:01 +0100

    final

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_222800) do
+ActiveRecord::Schema.define(version: 2018_07_31_131553) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "bio"
+    t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
